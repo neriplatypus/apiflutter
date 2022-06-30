c70d3750166993bd87ffe474e1c0696e7da84ef4
neriplatypus <neriplatypus@gmail.com>
2022-07-01 00:56:27 +0700

    Commit

---
+++ data_keuangan.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const dataKeuangan = require("./models/data.json");
+
+app.get('/', (req, res) => {
+    res.send(dataKeuangan);
+});
+
+app.listen(port, () => {
+
+    console.log(`Server is running on port ${port}`);
+
+});
