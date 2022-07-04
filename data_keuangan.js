ad3417c5afc06b4e4f2695836b7bdc7ff4b37f2c
neriplatypus <neriplatypus@gmail.com>
2022-07-04 20:41:00 +0700

    Commit

--- data_keuangan.js
+++ data_keuangan.js
@@ -1,14 +1,48 @@
+const bodyParser = require('body-parser');
 const express = require('express');
-
+const { uuid } = require('uuidv4');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-const dataKeuangan = require("./models/data.json");
-
+const dataKeuangan = []
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
-    res.send(dataKeuangan);
+    res.send(dataTiket);
 });
+
+app.post('/', (req, res) => {
+    const data = req.body
+    console.log(data);
+    dataKeuangan.push({id: uuid(), ...data});
+    res.send("Berhasil Terkirim!");
+});
+
+app.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    const data = dataKeuangan.find(data => data.id == id)
+    if (data) {
+        dataKeuangan.splice(dataKeuangan.indexOf(data), 1);
+    }
+
+    console.log(dataKeuangan);
+
+    res.send('Berhasil hapus data dengan id ${id}');
+});
+
+app.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    const { panel, tagihan, status } = req.body;
+    const data = dataKeuangan.find((data) => data.id == id);
+    if (panel) data.panel = panel;
+    if (tagihan) data.tagihan = tagihan;
+    if (status) data.status = status;
+
+    console.log('dataKeuangan', dataKeuangan);
+
+    res.send('Berhasil Update data dengan id ${id}');
+})
 
 app.listen(port, () => {
 
